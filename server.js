872f24eab9257f7ae72eaac40df57aef48f57593
Yire Morlans <yire.m@icloud.com>
2022-07-05 16:56:34 -0400

    mongo connection and database varaibles

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+//REQUIRE DEPENDENCIES
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'superdevs',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log(`Connected to ${dbName} database.`)
+        db = client.db(dbName)
+        collection = db.collection('superdevs')
+    })
+
+//VARIABLES FOR SETTING MIDDLEWEAR
