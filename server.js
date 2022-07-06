42a83f2720dbe131eb983228cd4e2b3c4ceaf2f5
Yire Morlans <yire.m@icloud.com>
2022-07-05 22:58:55 -0400

    add middleware save

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ MongoClient.connect(dbConnectionString)
         collection = db.collection('superdevs')
     })
 //VARIABLES FOR SETTING MIDDLEWEAR
+app.set('view engine', 'ejs') 
+app.use(express.static('public')) //all client side files 
+app.use(express.urlencoded({extended:true})) //parse urls that are being sent back and forth
+app.use(express.json()) //help express parse and read that data sent
+app.use(cors())
+
+
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Listening on port.`)
