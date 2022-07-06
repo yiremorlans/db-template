1c35138d96350ded3fa86b5b15dd2b0e4d211a92
Yire Morlans <yire.m@icloud.com>
2022-07-05 22:58:34 -0400

    add middleware

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config() 
 
-
+//SET UP DATABASE VARIABLES
 let db,
     dbConnectionString = process.env.DB_STRING,
     dbName = 'superdevs',
@@ -17,8 +17,8 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('superdevs')
     })
+//VARIABLES FOR SETTING MIDDLEWEAR
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Listening on port.`)
 })
-//VARIABLES FOR SETTING MIDDLEWEAR
